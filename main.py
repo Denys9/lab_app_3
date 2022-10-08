e9daf153684eed9354266c37c3924317320b3574
lozin <lozindenis6@gmail.com>
2022-10-08 10:55:48 +0300

    Create work "№2"

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ try:
     num = int(input('Введіть перше число - '))
     print(f'!{num}=', end='')
     temp = 1
-    for x in range (num1, num2+1):
-        suma+=x
-
-    else:
-        print(f'{suma}')
+    for item in range (1, num+1):
+        if item == num:
+            print(item,end = '')
+        else:
+            print(f'{item}*',end = '')
+        temp*=item
+    print(f' = {temp}')
 except Exception as ex:
     print(f'Eror information: {ex}')
 finally:
