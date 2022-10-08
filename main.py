8f320916aa9fbb2bd6c22055f5f50f29e96ae1db
lozin <lozindenis6@gmail.com>
2022-10-08 10:13:08 +0300

    Create work "№2"

---
+++ main.py
@@ -0,0 +1,13 @@
+try:
+    num1 = int(input('Введіть перше число - '))
+    num2 = int(input('Введіть друге число - '))
+    suma = num1
+    for x in range (num1, num2):
+        num1_=1
+        suma+=num1
+    else:
+        print(f'{suma}')
+except Exception as ex:
+    print(f'Eror information: {ex}')
+finally:
+    print('Exit')
