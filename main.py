7320e278a50660a9e7a7c3bec7d08da1939600d9
lozin <lozindenis6@gmail.com>
2022-10-08 10:44:48 +0300

    Create work "№1"

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ try:
     num1 = int(input('Введіть перше число - '))
     num2 = int(input('Введіть друге число - '))
     suma = 0
+    counter = num2-num1+1
     for x in range (num1, num2+1):
-
         suma+=x
+        avg = suma/counter
+
     else:
-        print(f'{suma}')
+        print(f'{suma}\n{avg}')
+
 except Exception as ex:
     print(f'Eror information: {ex}')
 finally:
